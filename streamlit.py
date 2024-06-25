c6f2aa50b588dcbe4fc061a35bfb0384f98442ac
AllenFTan17 <160197754+AllenFTan17@users.noreply.github.com>
2024-06-25 11:26:27 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -20,21 +20,22 @@ def main():
     st.title('Machine Learning Model Deployment')
 
     # Add user input components for 5 features
-    temp = st.slider('temp', min_value=0.0, max_value=10.0, value=0.1)
-    temp_min = st.slider('temp_min', min_value=0.0, max_value=10.0, value=0.1)
-    temp_max = st.slider('temp_max', min_value=0.0, max_value=10.0, value=0.1)
-    pressure = st.slider('pressure', min_value=0.0, max_value=10.0, value=0.1)
-    humidity = st.slider('humidity', min_value=0.0, max_value=10.0, value=0.1)
-    wind_speed = st.slider('wind_speed', min_value=0.0, max_value=10.0, value=0.1)
-    wind_deg = st.slider('wind_deg', min_value=0.0, max_value=10.0, value=0.1)
-    rain_1h = st.slider('rain_1h', min_value=0.0, max_value=10.0, value=0.1)
-    rain_3h = st.slider('rain_3h', min_value=0.0, max_value=10.0, value=0.1)
-    rain_6h = st.slider('rain_6h', min_value=0.0, max_value=10.0, value=0.1)
-    rain_12h = st.slider('rain_12h', min_value=0.0, max_value=10.0, value=0.1)
-    rain_24h = st.slider('rain_24h', min_value=0.0, max_value=10.0, value=0.1)
-    clouds_all= st.slider('clouds_all', min_value=0.0, max_value=10.0, value=0.1)
-    weather_main = st.slider('weather_main', min_value=0.0, max_value=10.0, value=0.1)
-    weather_description = st.slider('weather_description', min_value=0.0, max_value=10.0, value=0.1)
+    temp = st.slider('temp', min_value=0.0, max_value=50.0, value=0.01)
+    temp_min = st.slider('temp_min', min_value=0.0, max_value=50.0, value=0.01)
+    temp_max = st.slider('temp_max', min_value=0.0, max_value=50.0, value=0.01)
+    pressure = st.slider('pressure', min_value=1000.0, max_value=1050.0, value=0.1)
+    humidity = st.slider('humidity', min_value=0.0, max_value=100.0, value=1)
+    wind_speed = st.slider('wind_speed', min_value=0.0, max_value=20.0, value=0.01)
+    wind_deg = st.slider('wind_deg', min_value=0.0, max_value=360.0, value=1)
+    rain_1h = st.slider('rain_1h', min_value=0.0, max_value=2.0, value=0.01)
+    rain_3h = st.slider('rain_3h', min_value=0.0, max_value=100.0, value=0.1)
+    rain_6h = st.slider('rain_6h', min_value=0.0, max_value=100.0, value=0.1)
+    rain_12h = st.slider('rain_12h', min_value=0.0, max_value=250.0, value=1)
+    rain_24h = st.slider('rain_24h', min_value=0.0, max_value=200.0, value=0.1)
+    clouds_all= st.slider('clouds_all', min_value=0.0, max_value=100.0, value=1)
+    weather_main = st.slider('weather_main', min_value=0.0, max_value=3.0, value=1)
+    weather_description = st.slider('weather_description', min_value=0.0, max_value=14.0, value=1)
+
 
     if st.button('Make Prediction'):
         features = [temp,temp_min,temp_max,pressure,humidity,wind_speed,wind_deg,rain_1h,rain_3h,rain_6h,rain_12h,rain_24h,clouds_all,weather_main,weather_description]
