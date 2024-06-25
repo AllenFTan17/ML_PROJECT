ed24e448266abf6c8dfd02aff3b7f986cfbc6d1c
AllenFTan17 <160197754+AllenFTan17@users.noreply.github.com>
2024-06-25 11:34:51 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -24,17 +24,17 @@ def main():
     temp_min = st.slider('temp_min', min_value=0.0, max_value=50.0, value=0.01)
     temp_max = st.slider('temp_max', min_value=0.0, max_value=50.0, value=0.01)
     pressure = st.slider('pressure', min_value=1000.0, max_value=1050.0, value=0.1)
-    humidity = st.slider('humidity', min_value=0.0, max_value=100.0, value=0.1)
+    humidity = st.slider('humidity', min_value=0.0, max_value=100.0, value=1.00)
     wind_speed = st.slider('wind_speed', min_value=0.0, max_value=20.0, value=0.01)
-    wind_deg = st.slider('wind_deg', min_value=0.0, max_value=360.0, value=0.1)
+    wind_deg = st.slider('wind_deg', min_value=0.0, max_value=360.0, value=1.00)
     rain_1h = st.slider('rain_1h', min_value=0.0, max_value=2.0, value=0.01)
     rain_3h = st.slider('rain_3h', min_value=0.0, max_value=100.0, value=0.1)
     rain_6h = st.slider('rain_6h', min_value=0.0, max_value=100.0, value=0.1)
     rain_12h = st.slider('rain_12h', min_value=0.0, max_value=250.0, value=0.1)
     rain_24h = st.slider('rain_24h', min_value=0.0, max_value=200.0, value=0.1)
     clouds_all= st.slider('clouds_all', min_value=0.0, max_value=100.0, value=0.1)
-    weather_main = st.slider('weather_main', min_value=0.0, max_value=3.0, value=0.1)
-    weather_description = st.slider('weather_description', min_value=0.0, max_value=14.0, value=0.1)
+    weather_main = st.slider('weather_main', min_value=0.0, max_value=3.0, value=1.00)
+    weather_description = st.slider('weather_description', min_value=0.0, max_value=14.0, value=1.00)
 
 
     if st.button('Make Prediction'):
